332d623755d82b8f404e7d70e2d260ef0993ed6c
Karl Sander <kall@kall.ws>
2019-07-31 21:28:30 +0200

    clean up document

--- graphql.framerfx/code/App.tsx
+++ graphql.framerfx/code/App.tsx
@@ -1,30 +1,28 @@
-import { Override, Data } from "framer";
-import { useState } from "react";
-import { useSpring } from "react-spring";
-import { Logo, Circle } from "./canvas";
+import { Override, Data } from "framer"
+import { useState } from "react"
 
 const data = Data({
-  rotate: 0,
-  rotateY: 0,
-  toggle: true
-});
+    rotate: 0,
+    rotateY: 0,
+    toggle: true,
+})
 
 export function Rotate(): Override {
-  return {
-    animate: { rotate: data.rotate },
-    onTap() {
-      data.rotate = data.rotate + 90;
+    return {
+        animate: { rotate: data.rotate },
+        onTap() {
+            data.rotate = data.rotate + 90
+        },
     }
-  };
 }
 
 export function Trip(): Override {
-  const [run, setRun] = useState(false);
+    const [run, setRun] = useState(false)
 
-  return {
-    onTap: () => {
-      setRun(r => !r);
-    },
-    opacity: run ? 1 : 0.5
-  };
+    return {
+        onTap: () => {
+            setRun(r => !r)
+        },
+        opacity: run ? 1 : 0.5,
+    }
 }
